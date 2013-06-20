18671de3561fca52b2a9a3104c7e4db9e5fc7678
EvertonSpohr <ever.spohr@hotmail.com>
2013-06-20 17:17:04 -0300

    Inserido a classe arquivos

---
+++ TrabalhoAED/Arquivos/Arquivos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ *  Classe para tratar o arquivo de entrada.
+ * 
+ *  Everton R. Spohr
+ */
+
+
+namespace TrabalhoAED.Arquivos
+{
+    class Arquivos
+    {
+
+
+    }
+}
